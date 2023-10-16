1c8dc73f7df6339e053775ef60ce0e80aafac8cb
Brian Feister <bfeister@salesforce.com>
2023-10-16 17:14:00 -0600

    revert testing changes in slider.tsx and selection.tsx

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,35 +1,41 @@
 import {
-    RangeSlider,
-    RangeSliderControl,
-    RangeSliderLabel,
-    RangeSliderMarker,
-    RangeSliderMarkerGroup,
-    RangeSliderOutput,
-    RangeSliderRange,
-    RangeSliderThumb,
-    RangeSliderTrack,
-} from '@ark-ui/react'
+	RangeSlider,
+	RangeSliderControl,
+	RangeSliderLabel,
+	RangeSliderMarker,
+	RangeSliderMarkerGroup,
+	RangeSliderOutput,
+	RangeSliderRange,
+	RangeSliderThumb,
+	RangeSliderTrack,
+} from "@ark-ui/react"
 
-import {css} from '../../styled-system/css'
+import { css } from "../../styled-system/css"
 
-import {events} from '../data/mockEvents'
+import { events } from "../data/mockEvents"
 
 const eventDates = events.map((event) => event.eventDate)
 const earliestDate = Number(new Date(Math.min(...eventDates.map(Number))))
 const latestDate = new Date(Math.max(...eventDates.map(Number)))
 
 export const DateSlider = () => {
-    // return <>test!</>
-    return (
-        <RangeSlider>
-            <RangeSliderLabel>Label</RangeSliderLabel>
-            <RangeSliderControl>
-                <RangeSliderTrack>
-                    <RangeSliderRange />
-                </RangeSliderTrack>
-                <RangeSliderThumb index={0} />
-                <RangeSliderThumb index={1} />
-            </RangeSliderControl>
-        </RangeSlider>
-    )
+	return (
+		<RangeSlider
+			min={+earliestDate}
+			max={+latestDate}
+			className={css({
+				minHeight: "10rem",
+				display: "flex",
+			})}
+		>
+			<RangeSliderLabel>Label</RangeSliderLabel>
+			<RangeSliderControl>
+				<RangeSliderTrack className={css({height: "10px", bg: "black"})}>
+					<RangeSliderRange />
+				</RangeSliderTrack>
+				<RangeSliderThumb index={0} />
+				<RangeSliderThumb index={1} />
+			</RangeSliderControl>
+		</RangeSlider>
+	)
 }
