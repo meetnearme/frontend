0ca6eda5f1f4c52da1bc48bfdac02e7f9205993d
Brian Feister <bfeister@salesforce.com>
2023-10-16 16:56:55 -0600

    add resolution / overrides directives for yarn / pnpm

--- src/components/selection.tsx
+++ src/components/selection.tsx
@@ -1,38 +1,38 @@
-import { Pill } from "./pill"
-// import { DateSlider } from "./slider"
-import { cva } from "../../styled-system/css"
-import { styled } from "../../styled-system/jsx"
+import {Pill} from './pill'
+import {DateSlider} from './slider'
+import {cva} from '../../styled-system/css'
+import {styled} from '../../styled-system/jsx'
 
 const selectionStyle = cva({
-	base: {
-		border: "1px solid black",
-    padding: "0.75em",
-	},
+    base: {
+        border: '1px solid black',
+        padding: '0.75em',
+    },
 })
 
 const categoryListStyle = cva({
-	base: {
-		display: "flex",
-		justifyContent: "center",
-		flexWrap: "wrap",
-		gap: "1em",
-	},
+    base: {
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        gap: '1em',
+    },
 })
 
-const CategoryList = styled("div", categoryListStyle)
+const CategoryList = styled('div', categoryListStyle)
 
-const SelectionBox = styled("div", selectionStyle)
+const SelectionBox = styled('div', selectionStyle)
 
-const categories = ["Category A", "Category B", "Category C", "Category D"]
+const categories = ['Category A', 'Category B', 'Category C', 'Category D']
 
 export const Selection = () => (
-	<SelectionBox>
-		<h2>Categories</h2>
-		<CategoryList>
-			{categories.map((el) => (
-				<Pill key={el}>{el}</Pill>
-			))}
-		</CategoryList>
-		{/* <DateSlider /> */}
-	</SelectionBox>
+    <SelectionBox>
+        <h2>Categories</h2>
+        <CategoryList>
+            {categories.map((el) => (
+                <Pill key={el}>{el}</Pill>
+            ))}
+        </CategoryList>
+        <DateSlider />
+    </SelectionBox>
 )
--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,41 +1,35 @@
 import {
-	RangeSlider,
-	RangeSliderControl,
-	RangeSliderLabel,
-	RangeSliderMarker,
-	RangeSliderMarkerGroup,
-	RangeSliderOutput,
-	RangeSliderRange,
-	RangeSliderThumb,
-	RangeSliderTrack,
-} from "@ark-ui/react"
+    RangeSlider,
+    RangeSliderControl,
+    RangeSliderLabel,
+    RangeSliderMarker,
+    RangeSliderMarkerGroup,
+    RangeSliderOutput,
+    RangeSliderRange,
+    RangeSliderThumb,
+    RangeSliderTrack,
+} from '@ark-ui/react'
 
-import { css } from "../../styled-system/css"
+import {css} from '../../styled-system/css'
 
-import { events } from "../data/mockEvents"
+import {events} from '../data/mockEvents'
 
 const eventDates = events.map((event) => event.eventDate)
 const earliestDate = Number(new Date(Math.min(...eventDates.map(Number))))
 const latestDate = new Date(Math.max(...eventDates.map(Number)))
 
 export const DateSlider = () => {
-	return (
-		<RangeSlider
-			min={+earliestDate}
-			max={+latestDate}
-			className={css({
-				minHeight: "10rem",
-				display: "flex",
-			})}
-		>
-			<RangeSliderLabel>Label</RangeSliderLabel>
-			<RangeSliderControl>
-				<RangeSliderTrack className={css({height: "10px", bg: "black"})}>
-					<RangeSliderRange />
-				</RangeSliderTrack>
-				<RangeSliderThumb index={0} />
-				<RangeSliderThumb index={1} />
-			</RangeSliderControl>
-		</RangeSlider>
-	)
+    // return <>test!</>
+    return (
+        <RangeSlider>
+            <RangeSliderLabel>Label</RangeSliderLabel>
+            <RangeSliderControl>
+                <RangeSliderTrack>
+                    <RangeSliderRange />
+                </RangeSliderTrack>
+                <RangeSliderThumb index={0} />
+                <RangeSliderThumb index={1} />
+            </RangeSliderControl>
+        </RangeSlider>
+    )
 }
