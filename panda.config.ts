506b6ebf930f847c526b0238e9275a1b93b42557
dvkr <v3dvkr@gmail.com>
2023-10-19 17:32:25 -0600

    fix config to work with NextJS

--- panda.config.ts
+++ panda.config.ts
@@ -1,18 +1,13 @@
-import { defineConfig } from "@pandacss/dev"
+import { defineConfig } from '@pandacss/dev'
 
 export default defineConfig({
-	// Whether to use css reset
-	preflight: true,
-	presets: ["@pandacss/preset-base", "@park-ui/presets"],
-	// Where to look for your css declarations
-	include: [
-		"./src/components/**/*.{ts,tsx,js,jsx}",
-		"./src/app/**/*.{ts,tsx,js,jsx}",
-	],
-
-	// Files to exclude
-	exclude: [],
-	jsxFramework: "react",
-	// The output directory for your css system
-	outdir: "styled-system",
+  preflight: true,
+  presets: ['@pandacss/dev/presets', '@park-ui/presets'],
+  include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
+  exclude: [],
+  theme: {
+    extend: {},
+  },
+  jsxFramework: 'react',
+  outdir: 'styled-system',
 })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,76 @@
-import { css } from "../../styled-system/css"
-
-import {SelectMenu} from "../components/SelectMenu"
+"use client"
+// import { Container } from "styled-system/jsx"
+import { Button } from "~/components/ui/button"
+import {
+	Radio,
+	RadioButtonGroup,
+	RadioControl,
+	RadioLabel,
+} from "~/components/ui/radio-button-group"
+import {
+	Slider,
+	SliderControl,
+	SliderMarker,
+	SliderMarkerGroup,
+	SliderRange,
+	SliderThumb,
+	SliderTrack,
+} from "~/components/ui/slider"
+import {
+	TabContent,
+	TabIndicator,
+	TabList,
+	TabTrigger,
+	Tabs,
+} from "~/components/ui/tabs"
+// import {Accordian} from "~/components/ui/accordian"
 
 export default function Home() {
 	return (
-		<div className={css({ fontSize: "16px", fontWeight: "bold" })}>
-      <SelectMenu />
+		// <Container py={{ base: '12', md: '16' }} maxW="7xl">
+		<div>
+			<Tabs defaultValue="button">
+				<TabList>
+					<TabTrigger value="button">Button</TabTrigger>
+					<TabTrigger value="radio">Radio Group</TabTrigger>
+					<TabTrigger value="slider">Range Slider</TabTrigger>
+					<TabIndicator />
+				</TabList>
+				<TabContent value="button">
+					<Button size="md">Hello Park UI</Button>
+				</TabContent>
+				<TabContent value="radio">
+					<RadioButtonGroup defaultValue="react">
+						{[
+							{ value: "S" },
+							{ value: "M" },
+							{ value: "L", disabled: true },
+							{ value: "XL" },
+						].map((option, id) => (
+							<Radio key={id} value={option.value} disabled={option.disabled}>
+								<RadioControl />
+								<RadioLabel>{option.value}</RadioLabel>
+							</Radio>
+						))}
+					</RadioButtonGroup>
+				</TabContent>
+				<TabContent value="slider">
+					<Slider min={0} max={100} defaultValue={33}>
+						<SliderControl>
+							<SliderTrack>
+								<SliderRange />
+							</SliderTrack>
+							<SliderThumb />
+						</SliderControl>
+						<SliderMarkerGroup>
+							<SliderMarker value={25}>25</SliderMarker>
+							<SliderMarker value={50}>50</SliderMarker>
+							<SliderMarker value={75}>75</SliderMarker>
+						</SliderMarkerGroup>
+					</Slider>
+				</TabContent>
+			</Tabs>
 		</div>
+		// </Container>
 	)
 }
