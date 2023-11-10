7be88eb1664e6d124eabaf86e57f93d9ee21e408
dvkr <v3dvkr@gmail.com>
2023-11-09 19:55:55 -0700

    work in progress to use mock api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,47 @@
-"use client"
-import { Container } from "styled-system/jsx"
-import { CardWrapper } from "~/components/card-wrapper"
-import { Card } from "~/components/card"
+// "use client"
+import { Container } from 'styled-system/jsx'
+import { CardWrapper } from '~/components/card-wrapper'
+import { Card } from '~/components/card'
 
 import { events } from "~/data/mockEvents"
+// async function getData() {
+// 	const res = await fetch('http://localhost:4000/all-events', {
+// 		next: { revalidate: 10 },
+// 	})
+// 	// The return value is *not* serialized
+// 	// You can return Date, Map, Set, etc.
 
-export default function Home() {
+// 	if (!res.ok) {
+// 		// This will activate the closest `error.js` Error Boundary
+// 		throw new Error('Failed to fetch data')
+// 	}
+
+// 	return res.json()
+// }
+
+export default async function Home() {
+	// const events = await getData()
+	// console.log({ events })
 	return (
-		<Container py={{ base: "12", md: "16" }} maxW="5xl">
-					<CardWrapper>
-						{events.map((el) => (
-							<Card
-								key={el.heading}
-								heading={el.heading}
-								eventDate={el.eventDate}
-								desc={el.desc}
-								categories={el.categories}
-							/>
-						))}
-					</CardWrapper>
-
+		<Container py={{ base: '12', md: '16' }} maxW="5xl">
+			<CardWrapper>
+				{events.map(
+					(el: {
+						heading: string
+						eventDate: Date
+						desc?: string
+						categories?: string[]
+					}) => (
+						<Card
+							key={el.heading}
+							heading={el.heading}
+							eventDate={el.eventDate}
+							desc={el.desc}
+							categories={el.categories}
+						/>
+					)
+				)}
+			</CardWrapper>
 		</Container>
 	)
 }
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,9 +1,9 @@
-'use client'
+// 'use client'
 
-import { Pill } from "../components/pill"
-import { cva, css } from "../../styled-system/css"
-import { styled } from "../../styled-system/jsx"
-import { Badge } from "~/components/ui/badge"
+import { Pill } from '../components/pill'
+import { cva, css } from '../../styled-system/css'
+import { styled } from '../../styled-system/jsx'
+import { Badge } from '~/components/ui/badge'
 
 type propType = {
 	heading: string
@@ -14,36 +14,35 @@ type propType = {
 
 const cardStyle = cva({
 	base: {
-		padding: "0.75em",
-		borderRadius: "sm",
-		border: "2px solid gray",
-		width: "100%",
-		display: "flex",
-		flexFlow: "column",
-		gap: "1rem",
+		padding: '0.75em',
+		borderRadius: 'sm',
+		border: '2px solid gray',
+		width: '100%',
+		display: 'flex',
+		flexFlow: 'column',
+		gap: '1rem',
 	},
 })
 
-const CardBox = styled("div", cardStyle)
+const CardBox = styled('div', cardStyle)
 
 export const Card = (props: propType) => (
 	<CardBox>
 		<h2>{props.heading}</h2>
 		<p>
-			{props.eventDate.toLocaleString(undefined, {
-				year: "numeric",
-				month: "long",
-				day: "numeric",
+			{new Date(props.eventDate).toLocaleString(undefined, {
+				year: 'numeric',
+				month: 'long',
+				day: 'numeric',
 			})}
 		</p>
-		{<p>Description: {props.desc ? props.desc : ""}</p>}
+		{<p>Description: {props.desc ? props.desc : ''}</p>}
 		{props.categories && (
-
-				<p>
-					{props.categories.map((category) => (
-						<Badge key={category}>{category}</Badge>
-					))}
-				</p>
+			<p>
+				{props.categories.map((category) => (
+					<Badge key={category}>{category}</Badge>
+				))}
+			</p>
 		)}
 	</CardBox>
 )
