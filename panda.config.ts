abb754d36ea17c2e51bdcfe30736f41e82d2a32d
Brian Feister <brian.feister@gmail.com>
2023-12-01 22:10:19 -0700

    Add Park UI theme config and missing dependency for panda as described in park-ui.com >> "copy config" >> "make it yours"

--- panda.config.ts
+++ panda.config.ts
@@ -1,18 +1,43 @@
-import { defineConfig } from '@pandacss/dev'
-import {badgeRecipe} from "./badgeRecipe"
+import {defineConfig} from '@pandacss/dev'
+import {createPreset} from '@park-ui/panda-preset'
+import {badgeRecipe} from './badgeRecipe'
 
 export default defineConfig({
-  preflight: true,
-  presets: ['@pandacss/dev/presets', '@park-ui/presets'],
-  include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
-  exclude: [],
-  theme: {
-    extend: {
-      recipes: {
-        badge: badgeRecipe
-      }
+    preflight: true,
+    presets: [
+        '@pandacss/preset-base',
+        createPreset({
+            accentColor: 'grass',
+            grayColor: 'mauve',
+            borderRadius: 'md',
+        }),
+    ],
+    include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
+    exclude: [],
+    theme: {
+        extend: {
+            recipes: {
+                badge: badgeRecipe,
+            },
+        },
     },
-  },
-  jsxFramework: 'react',
-  outdir: 'styled-system',
+    jsxFramework: 'react',
+    outdir: 'styled-system',
 })
+
+// import {defineConfig} from '@pandacss/dev'
+
+// export default defineConfig({
+//     preflight: true,
+//     presets: [
+//         '@pandacss/preset-base',
+//         createPreset({
+//             accentColor: 'grass',
+//             grayColor: 'mauve',
+//             borderRadius: 'md',
+//         }),
+//     ],
+//     include: ['./src/**/*.{js,jsx,ts,tsx}'],
+//     jsxFramework: 'react',
+//     outdir: 'styled-system',
+// })
